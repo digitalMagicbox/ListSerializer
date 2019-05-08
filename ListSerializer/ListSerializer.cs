5418c0b919efd4b335e1c90e9a1114f536b05c40
yasuo <pambox+github@gmail.com>
2019-05-08 22:51:17 +0900

    プロジェクト ファイルを追加します。

---
+++ ListSerializer/ListSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ListSerializer
+{
+    class ListSerializer
+    {
+        public static void SaveToList<T>(string path, List<T> objects)
+        {
+            using (var sw = new StreamWriter(path, false))
+            {
+                new XmlSerializer(typeof(List<T>)).Serialize(sw, objects);
+            }
+        }
+
+        public static List<T> LoadFromList<T>(string path)
+        {
+            using (var sr = new StreamReader(path))
+            {
+                return (List<T>)new XmlSerializer(typeof(List<T>)).Deserialize(sr);
+            }
+        }
+    }
+}
---
+++ ListSerializer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListSerializer
+{
+    class Program
+    {
+        private const string Path = @"Numeric.xml";
+
+        static void Main(string[] args)
+        {
+            var xml = new List<int>
+            {
+                1,2,3,4,5,6,7,8,9,10
+            };
+
+            ListSerializer.SaveToList<int>(Path, xml);
+
+            var loadXml = ListSerializer.LoadFromList<int>(Path);
+
+            foreach(var i in loadXml)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
